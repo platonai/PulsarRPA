5e82f2bcac59e280be589c37dd87bab999bea065
Vincent Zhang <ivincent.zhang@gmail.com>
2020-08-18 13:43:15 +0800

    Improvement: move destroyProcess from ChromeLauncher to Runtimes

--- pulsar-common/src/main/kotlin/ai/platon/pulsar/common/Runtimes.kt
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/Runtimes.kt
@@ -1,11 +1,15 @@
 package ai.platon.pulsar.common
 
 import org.apache.commons.lang3.SystemUtils
+import org.slf4j.LoggerFactory
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 import kotlin.streams.toList
 
 object Runtimes {
+    private val logger = LoggerFactory.getLogger(Runtimes::class.java)
 
     fun exec(name: String): List<String> {
         if (!SystemUtils.IS_OS_LINUX) {
@@ -37,5 +41,49 @@ object Runtimes {
 
     fun checkIfProcessRunning(pattern: String): Boolean {
         return countSystemProcess(pattern) > 0
+    }
+
+    fun destroyProcess(process: Process, shutdownWaitTime: Duration) {
+        val info = formatProcessInfo(process.toHandle())
+
+        process.children().forEach { destroyChildProcess(it) }
+
+        process.destroy()
+        try {
+            if (!process.waitFor(shutdownWaitTime.seconds, TimeUnit.SECONDS)) {
+                process.destroyForcibly()
+                process.waitFor(shutdownWaitTime.seconds, TimeUnit.SECONDS)
+            }
+
+            logger.info("Exit | {}", info)
+        } catch (e: InterruptedException) {
+            process.destroyForcibly()
+            throw e
+        } finally {
+        }
+    }
+
+    fun formatProcessInfo(process: ProcessHandle): String {
+        val info = process.info()
+        val user = info.user().orElse("")
+        val pid = process.pid()
+        val ppid = process.parent().orElseGet { null }?.pid()?.toString()?:"?"
+        val startTime = info.startInstant().orElse(null)
+        val cpuDuration = info.totalCpuDuration()?.orElse(null)
+        val cmdLine = info.commandLine().orElseGet { "" }
+
+        return String.format("%-8s %-6d %-6s %-25s %-10s %s", user, pid, ppid, startTime?:"", cpuDuration?:"", cmdLine)
+    }
+
+    private fun destroyChildProcess(process: ProcessHandle) {
+        process.children().forEach { destroyChildProcess(it) }
+
+        val info = formatProcessInfo(process)
+        process.destroy()
+        if (process.isAlive) {
+            process.destroyForcibly()
+        }
+
+        logger.debug("Exit | {}", info)
     }
 }
