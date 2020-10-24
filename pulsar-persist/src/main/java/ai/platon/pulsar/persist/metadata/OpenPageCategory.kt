c9bfc58ece168e08796f7d44fe325845fd53cfaf
Vincent Zhang <ivincent.zhang@gmail.com>
2020-10-24 19:43:32 +0800

    Fix: correct OpenPageCategory.toString

--- pulsar-persist/src/main/java/ai/platon/pulsar/persist/metadata/OpenPageCategory.kt
+++ pulsar-persist/src/main/java/ai/platon/pulsar/persist/metadata/OpenPageCategory.kt
@@ -3,4 +3,5 @@ package ai.platon.pulsar.persist.metadata
 class OpenPageCategory(val name: String, val symbol: String) {
     constructor(pageCategory: PageCategory): this(pageCategory.name, pageCategory.symbol())
     fun toPageCategory() = PageCategory.parse(name)
+    override fun toString() = "$name $symbol"
 }
