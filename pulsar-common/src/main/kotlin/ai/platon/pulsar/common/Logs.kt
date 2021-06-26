e67921f09ae9d7c5a936638a1f7c2f21efd4ab53
Platon AI <galaxy.eye@qq.com>
2021-06-26 13:53:23 +0800

    Imps: 1. improved logging

--- pulsar-common/src/main/kotlin/ai/platon/pulsar/common/Logs.kt
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/Logs.kt
@@ -5,11 +5,15 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 
-fun <T : Any> getLogger(clazz: KClass<T>) = LoggerFactory.getLogger(clazz.java)
+fun <T : Any> getLogger(clazz: KClass<T>) = getLogger(clazz, "")
 
-fun getLogger(any: Any): Logger = if (any is KClass<*>) {
-    LoggerFactory.getLogger(any.java)
-} else LoggerFactory.getLogger(any::class.java)
+fun <T : Any> getLogger(clazz: KClass<T>, postfix: String) = LoggerFactory.getLogger(clazz.java.name + postfix)
+
+fun getLogger(any: Any): Logger = getLogger(any, "")
+
+fun getLogger(any: Any, postfix: String): Logger = if (any is KClass<*>) {
+    LoggerFactory.getLogger(any.java.name + postfix)
+} else LoggerFactory.getLogger(any::class.java.name + postfix)
 
 fun getTracer(any: Any): Logger? = getLogger(any).takeIf { it.isTraceEnabled }
 
