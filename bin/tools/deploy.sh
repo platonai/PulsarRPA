360f4b30fc355375847dc3f6b20a8b137be7b6eb
Platon AI <galaxy.eye@qq.com>
2022-04-27 11:59:42 +0800

    Feature: 1. add a deploy script

--- bin/tools/deploy.sh
+++ bin/tools/deploy.sh
@@ -4,6 +4,8 @@ bin=$(dirname "$0")/..
 bin=$(cd "$bin">/dev/null || exit; pwd)
 APP_HOME=$(cd "$bin"/..>/dev/null || exit; pwd)
 
+mvn clean
+
 echo "Deploy the project ..."
 echo "Changing version ..."
 
@@ -11,9 +13,7 @@ SNAPSHOT_VERSION=$(head -n 1 "$APP_HOME/VERSION")
 VERSION=$(sed 's/\(.*\)-.*/\1/' <<< $SNAPSHOT_VERSION)
 echo "$VERSION" > "$APP_HOME"/VERSION
 for f in */pom.xml; do
-  echo "$f"
   sed -i "s/$SNAPSHOT_VERSION/$VERSION/" "$f"
 done
 
-mvn clean
 mvn deploy -Pall-modules -Pplaton-release -Pplaton-deploy
