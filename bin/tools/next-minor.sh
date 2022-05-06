df49f6aae0ca5962bd63119dd2d8be05d321c34b
Platon AI <galaxy.eye@qq.com>
2022-05-06 12:20:38 +0800

    Fix: 1. next-minor script replaces versions in README.adoc Imps: 1. improve release scripts

--- bin/tools/next-minor.sh
+++ bin/tools/next-minor.sh
@@ -5,13 +5,29 @@ bin=$(cd "$bin">/dev/null || exit; pwd)
 APP_HOME=$(cd "$bin"/..>/dev/null || exit; pwd)
 
 SNAPSHOT_VERSION=$(head -n 1 "$APP_HOME/VERSION")
-VERSION=$(sed 's/\(.*\)-.*/\1/' <<< $SNAPSHOT_VERSION)
+VERSION=${SNAPSHOT_VERSION//"-SNAPSHOT"/""}
 PREFIX=$(echo "$VERSION" | cut -d'.' -f1,2)
 MINOR_VERSION=$(echo "$VERSION" | cut -d'.' -f3)
 MINOR_VERSION=$(("$MINOR_VERSION" + 1))
 
-NEXT_VERSION="$PREFIX.$MINOR_VERSION-SNAPSHOT"
+NEXT_VERSION="$PREFIX.$MINOR_VERSION"
+NEXT_SNAPSHOT_VERSION="$NEXT_VERSION-SNAPSHOT"
 
-echo "New version: $NEXT_VERSION"
-echo "$NEXT_VERSION" > "$APP_HOME"/VERSION
-find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$NEXT_VERSION/" {} \;
+echo "New version: $NEXT_SNAPSHOT_VERSION"
+# VERSION file
+echo "$NEXT_SNAPSHOT_VERSION" > "$APP_HOME"/VERSION
+# README
+sed -i "s/\b$PREFIX.[0-9]\{1,\}\b/$NEXT_VERSION/g" "$APP_HOME/README.adoc";
+# pom.xml files
+find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$NEXT_SNAPSHOT_VERSION/" {} \;
+
+COMMENT="v"${$NEXT_SNAPSHOT_VERSION//"-SNAPSHOT"/""}
+
+echo "Ready to commit with comment: <$COMMENT>"
+read -p "Are you sure to continue?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git add .
+  git commit -m "$COMMENT"
+  git push
+fi
--- bin/tools/release.sh
+++ bin/tools/release.sh
@@ -4,17 +4,38 @@ bin=$(dirname "$0")/..
 bin=$(cd "$bin">/dev/null || exit; pwd)
 APP_HOME=$(cd "$bin"/..>/dev/null || exit; pwd)
 
-VERSION=$(head -n 1 "$APP_HOME/VERSION")
-RESTORE_FILES="pulsar-ql-common/pom.xml pulsar-ql/pom.xml pulsar-plugins/pulsar-protocol-playwright/pom.xml pulsar-all/pom.xml pulsar-spring-support/pom.xml pulsar-spring-support/pulsar-beans/pom.xml pulsar-spring-support/pulsar-boot/pom.xml pulsar-rest/pom.xml pulsar-client/pom.xml pulsar-app/pom.xml pulsar-app/pulsar-app-resources/pom.xml pulsar-app/pulsar-app-resources/pulsar-app-common-resources/pom.xml pulsar-app/pulsar-sites-support/pom.xml pulsar-app/pulsar-sites-support/pulsar-site-amazon/pom.xml pulsar-plugins/pom.xml pulsar-app/pulsar-sites-support/pulsar-site-cn-gov/pom.xml pom.xml pulsar-app/pulsar-qa/pom.xml pulsar-common/pom.xml pulsar-app/pulsar-tests/pom.xml pulsar-persist/pom.xml pulsar-third/pom.xml pulsar-app/pulsar-master/pom.xml pulsar-app/pulsar-examples/pom.xml pulsar-third/pulsar-boilerpipe/pom.xml pulsar-skeleton/pom.xml pulsar-resources/pom.xml VERSION pulsar-plugins/pulsar-filter/pom.xml pulsar-plugins/pulsar-parse/pom.xml pulsar-plugins/pulsar-protocol/pom.xml pulsar-plugins/pulsar-scoring/pom.xml pulsar-dom/pom.xml pulsar-plugins/pulsar-index/pom.xml pulsar-plugins/pulsar-schedule/pom.xml pulsar-tools/pom.xml pulsar-tools/pulsar-browser/pom.xml"
+SNAPSHOT_VERSION=$(head -n 1 "$APP_HOME/VERSION")
+VERSION=${SNAPSHOT_VERSION//"-SNAPSHOT"/""}
 LAST_COMMIT_ID=$(git log --format="%H" -n 1)
 BRANCH=$(git branch --show-current)
+TAG="v$VERSION"
 
-read -p "Are you sure to release? " -n 1 -r
+echo "Ready to checkout branch $BRANCH"
+read -p "Are you sure to continue? " -n 1 -r
 echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-  echo ""
-then
-  git -c credential.helper= -c core.quotepath=false -c log.showSignature=false checkout "$BRANCH" -- "$RESTORE_FILES"
-  git -c credential.helper= -c core.quotepath=false -c log.showSignature=false tag v"$VERSION" "$LAST_COMMIT_ID"
-  git -c credential.helper= -c core.quotepath=false -c log.showSignature=false push --progress --porcelain origin refs/heads/"$BRANCH":"$BRANCH" --tags
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git checkout "$BRANCH"
+else
+  echo "Bye."
+  exit 0
+fi
+
+echo "Ready to add tag $TAG on $LAST_COMMIT_ID"
+read -p "Are you sure to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git tag "$TAG" "$LAST_COMMIT_ID"
+else
+  echo "Bye."
+  exit 0
+fi
+
+echo "Ready to push with tags to $BRANCH"
+read -p "Are you sure to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git push --tags
+else
+  echo "Bye."
+  exit 0
 fi
