8335866bcad8daa8bd1efe39eec54a4f6a11282f
galaxyeye <ivincent.zhang@gmail.com>
2022-08-01 13:56:40 +0800

    Imps: 1. add ResourceWalker to walk directories both in IDE and jar

---
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/ResourceWalker.kt
@@ -0,0 +1,54 @@
+package ai.platon.pulsar.common
+
+import ai.platon.pulsar.common.ResourceLoader
+import ai.platon.pulsar.common.getLogger
+import java.nio.file.*
+
+/**
+ * TODO: use ai.platon.pulsar.common.ResourceWalker instead when pulsar-1.9.11 or later available
+ * */
+class ResourceWalker {
+    companion object {
+        val SPRING_PACKEDR_ESOURCE_PREFIX = "BOOT-INF/classes/"
+    }
+
+    private val logger = getLogger(this)
+    // When the jar is packed using spring-boot, the resources are put into directory BOOT-INF/classes
+    var resourcePrefix: String = ""
+
+    init {
+        val uri = ResourceLoader.getResource(SPRING_PACKEDR_ESOURCE_PREFIX)?.toURI()
+        if (uri != null && uri.scheme == "jar") {
+            resourcePrefix = SPRING_PACKEDR_ESOURCE_PREFIX
+        }
+    }
+
+    fun walk(resourceBase: String, maxDepth: Int, visitor: (Path) -> Unit) {
+        val uri = ResourceLoader.getResource(resourceBase)?.toURI() ?: return
+
+        var fileSystem: FileSystem? = null
+        try {
+            val path = if (uri.scheme == "jar") {
+                val env: MutableMap<String, String> = HashMap()
+                fileSystem = FileSystems.newFileSystem(uri, env)
+                fileSystem.getPath("$resourcePrefix$resourceBase")
+            } else {
+                Paths.get(uri)
+            }
+
+            val walk = Files.walk(path, maxDepth)
+            val it = walk.iterator()
+            while (it.hasNext()) {
+                try {
+                    visitor(it.next())
+                } catch (t: Throwable) {
+                    logger.warn("Failed to visit path | $path", t)
+                }
+            }
+        } catch (t: Throwable) {
+            logger.warn("Unexpected failure to walk | $resourceBase", t)
+        } finally {
+            fileSystem?.close()
+        }
+    }
+}
