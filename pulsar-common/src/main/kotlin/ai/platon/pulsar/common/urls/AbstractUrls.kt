e7861b35ea716c7eb8347298f661b64e9360fbe5
galaxyeye <ivincent.zhang@gmail.com>
2022-09-19 11:37:24 +0800

    Imps: 1. refine url relative code

---
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/urls/AbstractUrls.kt
@@ -0,0 +1,100 @@
+package ai.platon.pulsar.common.urls
+
+import ai.platon.pulsar.common.DateTimes
+import ai.platon.pulsar.common.ResourceStatus
+import ai.platon.pulsar.common.config.AppConstants
+import ai.platon.pulsar.common.options.OptionUtils
+import java.net.MalformedURLException
+import java.net.URL
+import java.time.Instant
+
+
+abstract class AbstractUrl(
+    override var url: String,
+    override var args: String? = null,
+    override var referer: String? = null,
+    override var href: String? = null,
+    override var priority: Int = 0
+) : ComparableUrlAware {
+
+    override val configuredUrl get() = UrlUtils.mergeUrlArgs(url, args)
+
+    override val isStandard get() = UrlUtils.isValidUrl(url)
+
+    @get:Throws(MalformedURLException::class)
+    override val toURL get() = URL(url)
+
+    override val toURLOrNull get() = UrlUtils.getURLOrNull(url)
+
+    override val isNil get() = url == AppConstants.NIL_PAGE_URL
+
+    /**
+     * If this url is persistable
+     * */
+    override val isPersistable: Boolean = true
+
+    override val label: String get() = OptionUtils.findOption(args, listOf("-l", "-label", "--label")) ?: ""
+
+    override val deadTime: Instant
+        get() {
+            val deadTime = OptionUtils.findOption(args, listOf("-deadTime", "--dead-time")) ?: ""
+            return DateTimes.parseBestInstantOrNull(deadTime) ?: DateTimes.doomsday
+        }
+
+    /**
+     * Required website language
+     * */
+    override var lang: String = "*"
+
+    /**
+     * Required website country
+     * */
+    override var country: String = "*"
+
+    /**
+     * Required website district
+     * */
+    override var district: String = "*"
+
+    /**
+     * The maximum retry times
+     * */
+    override var nMaxRetry: Int = 3
+
+    /**
+     * An abstract url can compare to one of the following types:
+     * 1. a [String]
+     * 2. a [URL]
+     * 3. a [UrlAware]
+     * */
+    override fun equals(other: Any?): Boolean {
+        if (other === this) {
+            return true
+        }
+
+        return when (other) {
+            is String -> url == other
+            is URL -> url == other.toString()
+            is UrlAware -> url == other.url
+            else -> false
+        }
+    }
+
+    override fun compareTo(other: UrlAware): Int {
+        return url.compareTo(other.url)
+    }
+
+    override fun hashCode() = url.hashCode()
+
+    override fun toString() = url
+}
+
+abstract class AbstractStatefulUrl(
+    url: String,
+    args: String? = null,
+    referer: String? = null
+) : AbstractUrl(url, args, referer), StatefulUrl {
+    override var status: Int = ResourceStatus.SC_CREATED
+    override var modifiedAt: Instant = Instant.now()
+    override val createdAt: Instant = Instant.now()
+}
---
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/urls/Urls.kt
@@ -0,0 +1,122 @@
+package ai.platon.pulsar.common.urls
+
+import java.net.MalformedURLException
+import java.net.URL
+import java.time.Instant
+
+/**
+ * A degenerate url can be used to perform non-fetching tasks in the main loop.
+ * */
+interface DegenerateUrl {
+
+}
+
+/**
+ * A URL represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web.
+ * A resource can be something as simple as a file or a directory, or it can be a reference to
+ * a more complicated object, such as a query to a database or to a search engine.
+ *
+ * In java, a [URL] object represents a URL.
+ * In Pulsar, a [UrlAware] object represents a URL with extra information telling the system
+ * how to fetch it.
+ * */
+interface UrlAware {
+    /**
+     * The url, it can be configured or not
+     * */
+    var url: String
+
+    /**
+     * The url args
+     * */
+    var args: String?
+
+    /**
+     * The referer(or referrer)
+     * */
+    var referer: String?
+
+    /**
+     * The hypertext reference, It defines the address of the document, which this time is linked from
+     * */
+    var href: String?
+
+    /**
+     * The priority
+     * */
+    var priority: Int
+
+    /**
+     * The configured url, always be "$url $args"
+     * */
+    val configuredUrl: String
+
+    /**
+     * If true, the url is a standard URL and can be converted to a java.Util.URL
+     * */
+    val isStandard: Boolean
+
+    /**
+     * Converted to a java.Util.URL
+     * */
+    @get:Throws(MalformedURLException::class)
+    val toURL: URL
+
+    /**
+     * Converted to a java.Util.URL, if the url is invalid, return null
+     * */
+    val toURLOrNull: URL?
+
+    /**
+     * An url is Nil if it equals to AppConstants.NIL_PAGE_URL
+     * */
+    val isNil: Boolean
+
+    /**
+     * If this link is persistable
+     * */
+    val isPersistable: Boolean
+
+    /**
+     * The url label, it should be a shortcut for -label option in args
+     * */
+    val label: String
+
+    /**
+     * The url label, it should be a shortcut for -deadTime option in args
+     * */
+    val deadTime: Instant
+
+    /**
+     * Required website language
+     * */
+    val lang: String
+
+    /**
+     * Required website country
+     * */
+    val country: String
+
+    /**
+     * Required website district
+     * */
+    val district: String
+
+    /**
+     * The maximum retry times
+     * */
+    val nMaxRetry: Int
+}
+
+interface ComparableUrlAware : UrlAware, Comparable<UrlAware>
+
+/**
+ * The StatefulUrl interface. A StatefulUrl is an UrlAware with status.
+ * */
+interface StatefulUrl : ComparableUrlAware {
+    var authToken: String?
+    var remoteAddr: String?
+    var status: Int
+    var modifiedAt: Instant
+    val createdAt: Instant
+}
