109032648ccec4ace8e99fc116adac6a821b7c6f
galaxyeye <ivincent.zhang@gmail.com>
2022-09-22 09:35:14 +0800

    Fix: 1. correct Spring bean config file

--- pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/emulator/BrowserResponseHandlerFactory.kt
+++ pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/emulator/BrowserResponseHandlerFactory.kt
@@ -2,18 +2,16 @@ package ai.platon.pulsar.protocol.browser.emulator
 
 import ai.platon.pulsar.common.config.CapabilityTypes
 import ai.platon.pulsar.common.config.ImmutableConfig
-import ai.platon.pulsar.protocol.browser.driver.WebDriverPoolManager
 import ai.platon.pulsar.protocol.browser.emulator.impl.BrowserResponseHandlerImpl
 
 class BrowserResponseHandlerFactory(
-        private val driverPoolManager: WebDriverPoolManager,
         private val immutableConfig: ImmutableConfig
 ) {
     private val reflectedHandler by lazy {
         val clazz = immutableConfig.getClass(
                 CapabilityTypes.BROWSER_RESPONSE_HANDLER, BrowserResponseHandlerImpl::class.java)
-        clazz.constructors.first { it.parameters.size == 2 }
-                .newInstance(driverPoolManager, immutableConfig) as BrowserResponseHandler
+        clazz.constructors.first { it.parameters.size == 1 }
+                .newInstance(immutableConfig) as BrowserResponseHandler
     }
 
     var specifiedHandler: BrowserResponseHandler? = null
