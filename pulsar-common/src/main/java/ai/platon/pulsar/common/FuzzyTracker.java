14436d903c35f91247c20ed4cad4f931f26b0271
galaxyeye <ivincent.zhang@gmail.com>
2023-02-03 11:02:20 +0800

    Imps: 1. improve comments

--- pulsar-common/src/main/java/ai/platon/pulsar/common/FuzzyTracker.java
+++ pulsar-common/src/main/java/ai/platon/pulsar/common/FuzzyTracker.java
@@ -16,54 +16,26 @@ public class FuzzyTracker<T extends Comparable<T>> {
 
     private Map<T, Double> trackees = new TreeMap<>();
 
-    /**
-     * <p>Constructor for FuzzyTracker.</p>
-     */
     public FuzzyTracker() {
     }
 
-    /**
-     * <p>size.</p>
-     *
-     * @return a int.
-     */
     public int size() {
         return trackees.size();
     }
 
-    /**
-     * <p>isEmpty.</p>
-     *
-     * @return a boolean.
-     */
     public boolean isEmpty() {
         return trackees.size() == 0;
     }
 
-    /**
-     * <p>remove.</p>
-     *
-     * @param t a T object.
-     * @return a double.
-     */
     public double remove(T t) {
         Double r = trackees.remove(t);
         return r == null ? 0.0 : r;
     }
 
-    /**
-     * <p>clear.</p>
-     */
     public void clear() {
         trackees.clear();
     }
 
-    /**
-     * <p>get.</p>
-     *
-     * @param t a T object.
-     * @return a double.
-     */
     public double get(T t) {
         Double sim = trackees.get(t);
 
@@ -73,22 +45,10 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return sim;
     }
 
-    /**
-     * <p>set.</p>
-     *
-     * @param t a T object.
-     * @param p a {@link ai.platon.pulsar.common.FuzzyProbability} object.
-     */
     public void set(T t, FuzzyProbability p) {
         set(t, p.floor());
     }
 
-    /**
-     * <p>set.</p>
-     *
-     * @param t a T object.
-     * @param sim a double.
-     */
     public void set(T t, double sim) {
         Double oldValue = trackees.get(t);
 
@@ -101,13 +61,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         }
     }
 
-    /**
-     * <p>inc.</p>
-     *
-     * @param t a T object.
-     * @param sim a double.
-     * @return a double.
-     */
     public double inc(T t, double sim) {
         Double oldSim = trackees.get(t);
 
@@ -124,13 +77,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return sim;
     }
 
-    /**
-     * <p>dec.</p>
-     *
-     * @param t a T object.
-     * @param sim a double.
-     * @return a double.
-     */
     public double dec(T t, double sim) {
         Double oldSim = trackees.get(t);
 
@@ -149,11 +95,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
     }
 
     // 寻找相似度值最大的项
-    /**
-     * <p>primaryKey.</p>
-     *
-     * @return a T object.
-     */
     public T primaryKey() {
         T lastType = null;
         Double lastSim = 0.0;
@@ -168,21 +109,10 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return lastType;
     }
 
-    /**
-     * <p>keySet.</p>
-     *
-     * @return a {@link java.util.Set} object.
-     */
     public Set<T> keySet() {
         return trackees.keySet();
     }
 
-    /**
-     * <p>keySet.</p>
-     *
-     * @param p a {@link ai.platon.pulsar.common.FuzzyProbability} object.
-     * @return a {@link java.util.Set} object.
-     */
     public Set<T> keySet(FuzzyProbability p) {
         Set<T> keys = new HashSet<T>();
 
@@ -195,13 +125,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return keys;
     }
 
-    /**
-     * <p>is.</p>
-     *
-     * @param key a T object.
-     * @param p a {@link ai.platon.pulsar.common.FuzzyProbability} object.
-     * @return a boolean.
-     */
     public boolean is(T key, FuzzyProbability p) {
         Double sim = trackees.get(key);
         if (sim == null) {
@@ -213,12 +136,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return p2.floor() >= p.floor();
     }
 
-    /**
-     * <p>maybe.</p>
-     *
-     * @param key a T object.
-     * @return a boolean.
-     */
     public boolean maybe(T key) {
         Double p = trackees.get(key);
         if (p == null) {
@@ -228,12 +145,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return FuzzyProbability.maybe(p);
     }
 
-    /**
-     * <p>veryLikely.</p>
-     *
-     * @param key a T object.
-     * @return a boolean.
-     */
     public boolean veryLikely(T key) {
         Double p = trackees.get(key);
         if (p == null) {
@@ -243,12 +154,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return FuzzyProbability.veryLikely(p);
     }
 
-    /**
-     * <p>mustBe.</p>
-     *
-     * @param key a T object.
-     * @return a boolean.
-     */
     public boolean mustBe(T key) {
         Double p = trackees.get(key);
         if (p == null) {
@@ -258,12 +163,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return FuzzyProbability.mustBe(p);
     }
 
-    /**
-     * <p>certainly.</p>
-     *
-     * @param key a T object.
-     * @return a boolean.
-     */
     public boolean certainly(T key) {
         Double p = trackees.get(key);
         if (p == null) {
@@ -273,7 +172,6 @@ public class FuzzyTracker<T extends Comparable<T>> {
         return FuzzyProbability.certainly(p);
     }
 
-    /** {@inheritDoc} */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
