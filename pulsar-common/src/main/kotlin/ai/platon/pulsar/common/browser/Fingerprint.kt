504f91c6a74b838cccf21c1a4711db5fd4e28198
PlatonAI <galaxy.eye@qq.com>
2023-08-04 17:33:24 +0800

    Fix: 1. properly handle symbolic links on Windows 2. correct fingerprint comparison

--- pulsar-common/src/main/kotlin/ai/platon/pulsar/common/browser/Fingerprint.kt
+++ pulsar-common/src/main/kotlin/ai/platon/pulsar/common/browser/Fingerprint.kt
@@ -39,7 +39,7 @@ data class Fingerprint(
             userAgent to other.userAgent,
         ).forEach {
             r = comp.compare(it.first, it.second)
-println(it.first + " " + it.second + " " + r)
+
             if (r != 0) {
                 return r
             }
