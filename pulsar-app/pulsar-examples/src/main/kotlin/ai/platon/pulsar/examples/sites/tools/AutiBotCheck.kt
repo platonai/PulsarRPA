a3b055abc710532df53f212b7ef2cd957dd4e474
PlatonAI <galaxy.eye@qq.com>
2023-09-13 18:06:45 +0800

    Imps: 1. OpenChrome now opens chrome with prototype user data dir Docs: 1. add links of bot checking blogs and tools

--- pulsar-app/pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/tools/AutiBotCheck.kt
+++ pulsar-app/pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/tools/AutiBotCheck.kt
@@ -4,6 +4,15 @@ import ai.platon.pulsar.common.proxy.ProxyPool
 import ai.platon.pulsar.examples.sites.tools.proxy.TemporaryProxyLoader
 import ai.platon.pulsar.ql.context.SQLContexts
 
+/**
+ * Bot check blogs and tools:
+ *
+ * https://amiunique.org/
+ * https://gologin.com/check-browser
+ * https://browserleaks.com/canvas
+ * https://browserleaks.com/
+ * https://privacybee.com/blog/browser-fingerprinting/
+ * */
 fun main() {
     val urls = """
 http://www.baidu.com
@@ -14,14 +23,14 @@ https://arh.antoinevastel.com/bots/areyouheadless
         .map { it.trim() }
         .filter { it.startsWith("http") }
         .take(1)
-
+    
     val session = SQLContexts.createSession()
-
+    
     val proxyPool = session.context.getBean(ProxyPool::class)
     val proxyLoader = TemporaryProxyLoader(proxyPool)
     proxyLoader.loadProxies()
-
+    
     urls.forEach { session.open(it) }
-
+    
     readLine()
 }
--- pulsar-app/pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/tools/OpenChrome.kt
+++ pulsar-app/pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/tools/OpenChrome.kt
@@ -1,15 +1,16 @@
 package ai.platon.pulsar.examples.sites.tools
 
+import ai.platon.pulsar.crawl.fetch.privacy.BrowserId
 import ai.platon.pulsar.protocol.browser.driver.WebDriverFactory
 import ai.platon.pulsar.ql.context.SQLContexts
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    val driver = SQLContexts.create().getBean(WebDriverFactory::class).launchBrowser().newDriver()
-
+    val driver = SQLContexts.create().getBean(WebDriverFactory::class).launchBrowser(BrowserId.PROTOTYPE).newDriver()
+    
     runBlocking {
         driver.navigateTo("about:blank")
     }
-
+    
     readLine()
 }
