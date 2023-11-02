6c8b4c4ddaa0d0a1ef0f68f949477d4bd97f69a2
PlatonAI <galaxyeye@live.cn>
2023-11-03 00:53:37 +0800

    Imps: 1. disable FetchEntry's state locks

--- pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/driver/cdt/detail/ChromeNavigateEntry.kt
+++ pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/driver/cdt/detail/ChromeNavigateEntry.kt
@@ -10,58 +10,70 @@ class ChromeNavigateEntry(
     private val navigateEntry: NavigateEntry
 ) {
     private val logger = getLogger(this)
-    
+
     private val tracer = logger.takeIf { it.isTraceEnabled }
-    
+
     fun updateStateBeforeRequestSent(event: RequestWillBeSent) {
-        navigateEntry.synchronized { updateStateBeforeRequestSent0(event) }
+        // We may have better solution to do this
+//        if (!navigateEntry.documentTransferred) {
+//            navigateEntry.synchronized { updateStateBeforeRequestSent0(event) }
+//        } else {
+//            updateStateBeforeRequestSent0(event)
+//        }
+        updateStateBeforeRequestSent0(event)
     }
-    
+
     fun updateStateAfterResponseReceived(event: ResponseReceived) {
-        navigateEntry.synchronized { updateStateAfterResponseReceived0(event) }
+        // We may have better solution to do this
+//        if (!navigateEntry.documentTransferred) {
+//            navigateEntry.synchronized { updateStateAfterResponseReceived0(event) }
+//        } else {
+//            updateStateAfterResponseReceived0(event)
+//        }
+        updateStateAfterResponseReceived0(event)
     }
-    
+
     fun isMinorResource(event: RequestWillBeSent): Boolean {
         return navigateEntry.documentTransferred && isMinorResource(event.type)
     }
-    
+
     private fun updateStateBeforeRequestSent0(event: RequestWillBeSent) {
         val count = navigateEntry.networkRequestCount.incrementAndGet()
-        
+
         // TODO: handle redirection
-        
+
         // The first request, it should be the main HTML document
-        if (count == 1 && event.type != ResourceType.DOCUMENT) {
+        if (logger.isDebugEnabled && count == 1 && event.type != ResourceType.DOCUMENT) {
             // It might be a redirection, prefetch, or just an image
             var url = event.request.url
             if (url.startsWith("data:")) {
                 url = "data:xxx(...ignored)"
             }
-            logger.info(
+            logger.debug(
                 "The resource type of the first request is {}, requests: {} | {}",
                 event.type, navigateEntry.networkRequestCount, url
             )
         }
-        
+
         if (isMajorRequestWillBeSent(event)) {
             navigateEntry.updateMainRequest(event.requestId, event.request.headers)
         }
     }
-    
+
     private fun updateStateAfterResponseReceived0(event: ResponseReceived) {
         val count = navigateEntry.networkResponseCount.incrementAndGet()
         val response = event.response
-        
+
         // TODO: handle redirection
-        
+
         // The first response, it should be the main HTML document
-        if (count == 1 && event.type != ResourceType.DOCUMENT) {
+        if (logger.isDebugEnabled && count == 1 && event.type != ResourceType.DOCUMENT) {
             var url = response.url
             if (url.startsWith("data:")) {
                 url = "data:xxx(...ignored)"
             }
             // It might be a redirection, prefetch, or just an image
-            logger.info("The resource type of the first response is {}, responses: {} | {}",
+            logger.debug("The resource type of the first response is {}, responses: {} | {}",
                 event.type, navigateEntry.networkResponseCount, url)
         }
 
@@ -74,11 +86,11 @@ class ChromeNavigateEntry(
     private fun isMajorRequestWillBeSent(event: RequestWillBeSent): Boolean {
         return !navigateEntry.documentTransferred && event.type == ResourceType.DOCUMENT
     }
-    
+
     private fun isMajorResponseReceived(event: ResponseReceived): Boolean {
         return !navigateEntry.documentTransferred && event.type == ResourceType.DOCUMENT
     }
-    
+
     private fun isMinorResource(type: ResourceType): Boolean {
         return type in listOf(
             ResourceType.FONT,
