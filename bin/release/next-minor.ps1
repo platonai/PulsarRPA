e1ca617cea2cf6dbaa7b07e58b1ea09a78c37447
PlatonAI <galaxy.eye@qq.com>
2024-03-13 21:39:00 +0800

    Feature: 1. add release tools in powershell

---
+++ bin/release/next-minor.ps1
@@ -0,0 +1,49 @@
+# Set up variables
+$bin = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$APP_HOME = (Resolve-Path "$bin\..").Path
+
+# Get version information
+$SNAPSHOT_VERSION = Get-Content "$APP_HOME\VERSION" -TotalCount 1
+$VERSION =$SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
+$PREFIX =$VERSION.Split('.')[0,2] -join '.'
+$MINOR_VERSION =$VERSION.Split('.')[2]
+$MINOR_VERSION = [int]$MINOR_VERSION + 1
+
+$NEXT_VERSION = "$PREFIX.$MINOR_VERSION"$NEXT_SNAPSHOT_VERSION = "$NEXT_VERSION-SNAPSHOT"
+
+# Output new version
+Write-Host "New version: $NEXT_SNAPSHOT_VERSION"
+
+# Update VERSION file
+$NEXT_SNAPSHOT_VERSION | Set-Content "$APP_HOME\VERSION"
+
+# Update $APP_HOME/pom.xml$pomXmlPath = "$APP_HOME\pom.xml"
+((Get-Content $pomXmlPath) -replace "<tag>v$VERSION<\/tag>", "<tag>v$NEXT_VERSION<\/tag>") | Set-Content $pomXmlPath
+
+# Update pom.xml files
+Get-ChildItem "$APP_HOME" -Filter 'pom.xml' -Recurse | ForEach-Object {
+  ((Get-Content $_.FullName) -replace$SNAPSHOT_VERSION, $NEXT_SNAPSHOT_VERSION) | Set-Content$_.FullName
+}
+
+# Files containing the version number to upgrade
+$VERSION_AWARE_FILES = @(
+  "$APP_HOME\README.adoc"
+  "$APP_HOME\README-CN.adoc"
+)
+
+# Replace version numbers in files
+foreach ($F in$VERSION_AWARE_FILES) {
+  ((Get-Content $F) -replace "\b$PREFIX\.[0-9]+\b", $NEXT_VERSION) | Set-Content $F
+}
+
+# Commit comment
+$COMMENT =$NEXT_SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
+
+# Prompt for confirmation
+Write-Host "Ready to commit with comment: <$COMMENT>"
+$confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+if ($confirm -eq 'Y') {
+  git add .
+  git commit -m "$COMMENT"
+  git push
+}
---
+++ bin/release/release.ps1
@@ -0,0 +1,100 @@
+# Set up variables
+$bin = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$APP_HOME = (Resolve-Path "$bin\..").Path
+$gitExe = "git" # Assuming git is in the system PATH
+
+# Switching remote URLs from HTTPS to SSH
+& $gitExe remote set-url origin git@github.com:platonai/PulsarRPA.git
+
+# Get version information
+$SNAPSHOT_VERSION = Get-Content "$APP_HOME\VERSION" -TotalCount 1
+$VERSION =$SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
+$LAST_COMMIT_ID = &$gitExe log --format="%H" -n 1
+$BRANCH = &$gitExe branch --show-current
+$TAG = "v$VERSION"
+
+function Restore-WorkingBranch {
+  Write-Host "Ready to restore"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe restore .
+  } else {
+    Write-Host "Bye."
+    exit 0
+  }
+}
+
+function Pull-Changes {
+  Write-Host "Ready to pull"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe pull
+  } else {
+    Write-Host "Bye."
+    exit 0
+  }
+}
+
+function Add-Tag {
+  Write-Host "Ready to add tag $TAG on$LAST_COMMIT_ID"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe tag "$TAG" "$LAST_COMMIT_ID"
+    Push-WithTags
+  } else {
+    Write-Host "Do not add tag."
+  }
+}
+
+function Push-WithTags {
+  Write-Host "Ready to push with tags to $BRANCH"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe push --tags
+  } else {
+    Write-Host "Do not push with tags"
+  }
+}
+
+function Merge-ToMainBranch {
+  Write-Host "Ready to merge to main branch"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe checkout main
+    if ($LASTEXITCODE -ne 0) {
+      & $gitExe checkout master
+    }
+    & $gitExe merge "$BRANCH"
+    Push-ToMainBranch
+  } else {
+    Write-Host "Do not merge to main branch."
+  }
+}
+
+function Push-ToMainBranch {
+  Write-Host "Ready to push to main branch"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe push
+  } else {
+    Write-Host "Bye."
+    exit 0
+  }
+}
+
+function Checkout-WorkingBranch {
+  Write-Host "Ready to checkout working branch $BRANCH"
+  $confirm = Read-Host -Prompt "Are you sure to continue? [Y/n]"
+  if ($confirm -eq 'Y') {
+    & $gitExe checkout "$BRANCH"
+  } else {
+    Write-Host "Remain on main branch"
+  }
+}
+
+# Call functions
+Restore-WorkingBranch
+Pull-Changes
+Merge-ToMainBranch
+Checkout-WorkingBranch
+Add-Tag
