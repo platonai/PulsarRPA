7b35d39a7f2138cf761a12b6b31296deed895d5d
PlatonAI <galaxy.eye@qq.com>
2024-03-16 14:06:09 +0800

    Feature: 1. non-blocking communication with Chrome Devtools - 3 - failed, no easy way to employee coroutines with java reflections, remove all code for such attempt to reduce code size and third-party dependency

--- pulsar-tools/pulsar-browser/src/main/kotlin/ai/platon/pulsar/browser/driver/chrome/impl/TransportImpl.kt
+++ pulsar-tools/pulsar-browser/src/main/kotlin/ai/platon/pulsar/browser/driver/chrome/impl/TransportImpl.kt
@@ -8,10 +8,6 @@ import ai.platon.pulsar.common.brief
 import ai.platon.pulsar.common.config.AppConstants
 import ai.platon.pulsar.common.warnForClose
 import com.codahale.metrics.SharedMetricRegistries
-import io.ktor.client.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.http.*
-import io.ktor.websocket.*
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 import java.io.IOException
@@ -21,7 +17,6 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Consumer
 import javax.websocket.*
-import javax.websocket.CloseReason
 
 class TransportImpl : Transport {
     private val logger = LoggerFactory.getLogger(TransportImpl::class.java)
@@ -41,7 +36,7 @@ class TransportImpl : Transport {
             consumer.accept(message)
         }
     }
-
+    
     /**
      * Connect the supplied annotated endpoint instance to its server. The supplied
      * object must be a class decorated with the class level.
@@ -55,25 +50,25 @@ class TransportImpl : Transport {
     @Throws(WebSocketServiceException::class, IllegalStateException::class)
     override fun connect(uri: URI) {
         val webSocketService = this
-
+        
         val endpoint = object : Endpoint() {
             override fun onOpen(session: Session, config: EndpointConfig) {
                 webSocketService.onOpen(session, config)
                 logger.info("Connected to ws server {}", uri)
             }
-
+            
             override fun onClose(session: Session, closeReason: CloseReason) {
                 super.onClose(session, closeReason)
                 webSocketService.onClose(session, closeReason)
                 logger.info("Closing ws server {}", uri)
             }
-
+            
             override fun onError(session: Session, e: Throwable?) {
                 super.onError(session, e)
                 webSocketService.onError(session, e)
             }
         }
-
+        
         session = try {
             WEB_SOCKET_CONTAINER.connectToServer(endpoint, uri)
         } catch (e: DeploymentException) {
@@ -84,11 +79,11 @@ class TransportImpl : Transport {
             throw WebSocketServiceException("Failed connecting to ws server | $uri", e)
         }
     }
-
+    
     @Throws(WebSocketServiceException::class)
     override fun send(message: String) {
         meterRequests.mark()
-
+        
         try {
             tracer?.trace("Send {}", StringUtils.abbreviateMiddle(message, "...", 500))
             session.basicRemote.sendText(message)
@@ -102,11 +97,11 @@ class TransportImpl : Transport {
             logger.error("[Unexpected] | ${session.requestURI}", e)
         }
     }
-
+    
     @Throws(WebSocketServiceException::class)
     override fun sendAsync(message: String): Future<Void> {
         meterRequests.mark()
-
+        
         return try {
             session.asyncRemote.sendText(message)
         } catch (e: IOException) {
@@ -120,57 +115,57 @@ class TransportImpl : Transport {
             throw e
         }
     }
-
+    
     @Throws(WebSocketServiceException::class)
     override fun addMessageHandler(consumer: Consumer<String>) {
         if (session.messageHandlers.isNotEmpty()) {
             throw WebSocketServiceException("You are already subscribed to this web socket service.")
         }
-
+        
         session.addMessageHandler(DevToolsMessageHandler(consumer))
     }
-
+    
     private fun onOpen(session: Session, config: EndpointConfig) {
         logger.info("Connected to ws {}", session.requestURI)
     }
-
+    
     private fun onClose(session: Session, closeReason: CloseReason) {
         logger.info(
             "Web socket connection closed {} {}",
             closeReason.closeCode,
             closeReason.reasonPhrase
         )
-
+        
         if (WebSocketUtils.isTyrusBufferOverflowCloseReason(closeReason)) {
             logger.error(
                 "Web socket connection closed due to BufferOverflow raised by Tyrus client. This indicates the message "
-                        + "about to be received is larger than the incoming buffer in Tyrus client. "
-                        + "See DefaultWebSocketContainerFactory class source on how to increase the incoming buffer size in Tyrus or visit https://github.com/kklisura/chrome-devtools-java-client/blob/master/cdt-examples/src/main/java/com/github/kklisura/cdt/examples/IncreasedIncomingBufferInTyrusExample.java"
+                    + "about to be received is larger than the incoming buffer in Tyrus client. "
+                    + "See DefaultWebSocketContainerFactory class source on how to increase the incoming buffer size in Tyrus or visit https://github.com/kklisura/chrome-devtools-java-client/blob/master/cdt-examples/src/main/java/com/github/kklisura/cdt/examples/IncreasedIncomingBufferInTyrusExample.java"
             )
         }
     }
-
+    
     private fun onError(session: Session, e: Throwable?) {
         logger.error("WS session error | {}\n>>>{}<<<", session.requestURI, e?.brief())
     }
-
+    
     override fun close() {
         if (closed.compareAndSet(false, true)) {
             session.runCatching { close() }.onFailure { warnForClose(this, it) }
         }
     }
-
+    
     override fun toString(): String {
         return session.requestURI.toString()
     }
-
+    
     companion object {
         val instanceSequencer = AtomicInteger()
         private const val WEB_SOCKET_CONTAINER_FACTORY_PROPERTY =
             "ai.platon.pulsar.browser.driver.chrome.webSocketContainerFactory"
         private val DEFAULT_WEB_SOCKET_CONTAINER_FACTORY = DefaultWebSocketContainerFactory::class.java.name
         val WEB_SOCKET_CONTAINER = createWebSocketContainer()
-
+        
         /**
          * Creates a WebSocketService and connects to a specified uri.
          *
@@ -182,7 +177,7 @@ class TransportImpl : Transport {
         fun create(uri: URI): Transport {
             return TransportImpl().also { it.connect(uri) }
         }
-
+        
         /**
          * Returns a WebSocketContainer retrieved from class defined in system property
          * ai.platon.pulsar.browser.driver.chrome.webSocketContainerFactory.
@@ -193,7 +188,7 @@ class TransportImpl : Transport {
         private fun createWebSocketContainer(): WebSocketContainer {
             val className =
                 System.getProperty(WEB_SOCKET_CONTAINER_FACTORY_PROPERTY, DEFAULT_WEB_SOCKET_CONTAINER_FACTORY)
-
+            
             try {
                 return (Class.forName(className) as Class<WebSocketContainerFactory>).newInstance().wsContainer
             } catch (e: IllegalAccessException) {
--- pulsar-tools/pulsar-browser/src/test/kotlin/ai/platon/pulsar/browser/driver/ws/server/plugins/Routing.kt
+++
@@ -1,9 +0,0 @@
-package ai.platon.pulsar.browser.driver.ws.server.plugins
-
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
-
-fun Application.configureRouting() {
-    routing {
-    }
-}
