18a3bef6bb2d58aa968bd3d2c6f608c18648beba
PlatonAI <galaxy.eye@qq.com>
2024-07-14 22:24:13 +0800

    Fix: 1. correct pulsar-ql-common, make it shaded Imps: 1. back compatibility

---
+++ pulsar-skeleton/src/main/kotlin/ai/platon/pulsar/context/PulsarContexts.kt
@@ -0,0 +1,4 @@
+package ai.platon.pulsar.context
+
+@Deprecated("Use ai.platon.pulsar.skeleton.context.PulsarContexts instead")
+typealias PulsarContexts = ai.platon.pulsar.skeleton.context.PulsarContexts
---
+++ pulsar-skeleton/src/main/kotlin/ai/platon/pulsar/session/PulsarSession.kt
@@ -0,0 +1,4 @@
+package ai.platon.pulsar.session
+
+@Deprecated("Use ai.platon.pulsar.skeleton.session.PulsarSession instead")
+typealias PulsarSession = ai.platon.pulsar.skeleton.session.PulsarSession
