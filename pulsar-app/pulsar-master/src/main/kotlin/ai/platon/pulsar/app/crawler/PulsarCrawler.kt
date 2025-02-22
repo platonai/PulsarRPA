8036740c39b919823ff4599eb925f99ba93a60a7
platon.AI <galaxyeye@live.cn>
2025-02-22 16:05:08 +0800

    Fix: 1. correct PulsarCrawler

--- pulsar-app/pulsar-master/src/main/kotlin/ai/platon/pulsar/app/crawler/PulsarCrawler.kt
+++ pulsar-app/pulsar-master/src/main/kotlin/ai/platon/pulsar/app/crawler/PulsarCrawler.kt
@@ -2,31 +2,26 @@ package ai.platon.pulsar.app.crawler
 
 import ai.platon.pulsar.common.LinkExtractors
 import ai.platon.pulsar.common.urls.Hyperlink
-import ai.platon.pulsar.skeleton.crawl.common.GlobalCacheFactory
+import ai.platon.pulsar.skeleton.context.PulsarContexts
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
 @ImportResource("classpath:pulsar-beans/app-context.xml")
-class PulsarCrawler(
-    private val globalCacheFactory: GlobalCacheFactory
-) {
-    private val globalCache get() = globalCacheFactory.globalCache
-    private val urlCache get() = globalCache.urlPool.normalCache
+class PulsarCrawler {
+    private val session = PulsarContexts.createSession()
 
     @PostConstruct
     fun generate() {
         val resource = "seeds/amazon/best-sellers/leaf-categories.txt"
-        LinkExtractors.fromResource(resource).mapTo(urlCache.nReentrantQueue) { Hyperlink(it, "", args = "-i 1s") }
+        LinkExtractors.fromResource(resource).asSequence()
+            .map { Hyperlink(it, "", args = "-i 1s") }
+            .forEach { session.submit(it) }
     }
 }
 
 fun main(args: Array<String>) {
-    runApplication<PulsarCrawler>(*args) {
-        setRegisterShutdownHook(true)
-        setLogStartupInfo(true)
-    }
+    runApplication<PulsarCrawler>(*args)
 }
