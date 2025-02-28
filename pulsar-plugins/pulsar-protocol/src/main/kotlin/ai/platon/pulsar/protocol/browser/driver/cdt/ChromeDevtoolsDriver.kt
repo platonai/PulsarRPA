9f189d3b8d7a0e23d1eeb1ca0f21c1d5c5592226
galaxyeye <ivincent.zhang@gmail.com>
2025-02-28 23:03:08 +0800

    Imps: 1. improve local file handling in ChromeDevtoolsDriver refactor: replace LOCAL_FILE_SERVE_PREFIX with LOCAL_FILE_BASE_URL and update related methods

--- pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/driver/cdt/ChromeDevtoolsDriver.kt
+++ pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/driver/cdt/ChromeDevtoolsDriver.kt
@@ -94,7 +94,7 @@ class ChromeDevtoolsDriver(
     private val initScriptCache = mutableListOf<String>()
     private val closed = AtomicBoolean()
 
-    val isGone get() = closed.get() || isQuit || !AppContext.isActive || !devTools.isOpen
+    private val isGone get() = closed.get() || isQuit || !AppContext.isActive || !devTools.isOpen
     val isActive get() = !isGone
 
     /**
@@ -526,7 +526,7 @@ class ChromeDevtoolsDriver(
             null
         }
     }
-    
+
     /**
      * This method scrolls element into view if needed, and then uses
      * {@link page.captureScreenshot} to take a screenshot of the element.
@@ -703,7 +703,7 @@ class ChromeDevtoolsDriver(
         pageAPI?.onWindowOpen { onWindowOpen(it) }
         // pageAPI?.onFrameAttached {  }
 //        pageAPI?.onDomContentEventFired {  }
-        
+
         val proxyEntry = browser.id.fingerprint.proxyEntry
         if (proxyEntry?.username != null) {
             credentials = Credentials(proxyEntry.username!!, proxyEntry.password)
@@ -711,8 +711,12 @@ class ChromeDevtoolsDriver(
         }
 
         navigateUrl = url
-        // TODO: This is a temporary solution to serve local file, for example, file:///tmp/example.html
-        if (AppConstants.LOCAL_FILE_SERVE_PREFIX in url) {
+        if (UrlUtils.isLocalFile(url)) {
+            // serve local file, for example:
+            // local file path:
+            // C:\Users\pereg\AppData\Local\Temp\pulsar\test.txt
+            // converted to:
+            // http://localfile.org?path=QzpcVXNlcnNccGVyZWdcQXBwRGF0YVxMb2NhbFxUZW1wXHB1bHNhclx0ZXN0LnR4dA==
             openLocalFile(url)
         } else {
             page.navigate(url, referrer = navigateEntry.pageReferrer)
@@ -730,6 +734,12 @@ class ChromeDevtoolsDriver(
     }
 
     private fun openLocalFile(url: String) {
+        val path = UrlUtils.localURLToPath(url)
+        val uri = path.toUri()
+        page.navigate(uri.toString())
+    }
+
+    private fun openLocalFileDeprecated(url: String) {
         if (url.contains("?path=")) {
             val queryParams = URIBuilder(url).queryParams
             val path = queryParams.firstOrNull { it.name == "path" }?.value
@@ -740,7 +750,7 @@ class ChromeDevtoolsDriver(
             return
         }
 
-        val url0 = url.removePrefix(AppConstants.LOCAL_FILE_SERVE_PREFIX)
+        val url0 = url.removePrefix(AppConstants.LOCAL_FILE_BASE_URL)
         if (SystemUtils.IS_OS_WINDOWS) {
             page.navigate(url0)
         } else {
