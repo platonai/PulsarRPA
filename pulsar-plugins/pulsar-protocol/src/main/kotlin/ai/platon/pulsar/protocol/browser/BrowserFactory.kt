9f51944db079234fba74f3ee93eb0d6cc07e5117
platon.AI <galaxyeye@live.cn>
2025-03-02 23:31:34 +0800

    feat(protocol): 为 BrowserFactory 类添加文档注释
    - 为 BrowserFactory 类的各个方法添加了详细的文档注释
    - 注释使用了 Markdown 格式，简明扼要地说明了每个方法的功能
    - 通过注释提高了代码的可读性和可维护性

--- pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/BrowserFactory.kt
+++ pulsar-plugins/pulsar-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/BrowserFactory.kt
@@ -14,24 +14,45 @@ import ai.platon.pulsar.skeleton.crawl.fetch.privacy.BrowserId
 class BrowserFactory {
     private val impl = BrowserFactoryImpl()
 
+    /**
+     * Connect to a browser instance.
+     * */
     fun connect(port: Int, settings: BrowserSettings = BrowserSettings()): Browser = impl.connect(port, settings)
 
+    /**
+     * Launch the system default browser, the system default browser is your daily used browser.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launchSystemDefaultBrowser(): Browser = impl.launch(BrowserId.SYSTEM_DEFAULT, LauncherOptions(), ChromeOptions())
 
+    /**
+     * Launch the default browser, notice, the default browser is not the one you used daily.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launchDefaultBrowser(): Browser = impl.launch(BrowserId.DEFAULT, LauncherOptions(), ChromeOptions())
 
+    /**
+     * Launch the prototype browser, the prototype browser is a browser instance with default settings.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launchPrototypeBrowser(): Browser = impl.launch(BrowserId.PROTOTYPE, LauncherOptions(), ChromeOptions())
 
+    /**
+     * Launch the next sequential browser, the browser's user data dir rotates between a group of dirs.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launchNextSequentialBrowser(): Browser =
         impl.launch(BrowserId.NEXT_SEQUENTIAL, LauncherOptions(), ChromeOptions())
 
+    /**
+     * Launch a random temporary browser, the browser's user data dir is a random temporary dir.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launchRandomTempBrowser(): Browser = impl.launch(BrowserId.RANDOM, LauncherOptions(), ChromeOptions())
 
+    /**
+     * Launch a browser with the given browser id, the browser id is used to identify the browser instance.
+     * */
     @Throws(BrowserLaunchException::class)
     fun launch(
         browserId: BrowserId, launcherOptions: LauncherOptions, launchOptions: ChromeOptions
