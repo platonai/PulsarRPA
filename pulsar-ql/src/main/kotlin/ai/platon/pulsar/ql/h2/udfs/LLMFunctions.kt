ae50a4db2f7e9e236b2e071559b28d8bb38f440b
platon.AI <galaxyeye@live.cn>
2025-04-13 10:19:05 +0800

    feat(ql): 添加大型语言模型（LLM）相关函数

---
+++ pulsar-ql/src/main/kotlin/ai/platon/pulsar/ql/h2/udfs/LLMFunctions.kt
@@ -0,0 +1,52 @@
+package ai.platon.pulsar.ql.h2.udfs
+
+import ai.platon.pulsar.ql.common.annotation.UDFGroup
+import ai.platon.pulsar.ql.common.annotation.UDFunction
+import ai.platon.pulsar.ql.common.types.ValueDom
+import ai.platon.pulsar.skeleton.context.PulsarContexts
+
+@Suppress("unused")
+@UDFGroup(namespace = "LLM")
+object LLMFunctions {
+    private val session get() = PulsarContexts.getOrCreateSession()
+
+    @JvmStatic
+    @UDFunction(description = "Get the LLM model name")
+    fun modelName(): String {
+        return session.unmodifiedConfig["llm.name"] ?: "unknown"
+    }
+
+    @JvmStatic
+    @UDFunction(description = "Chat with the LLM model")
+    fun chat(prompt: String): String {
+        return session.chat(prompt).content
+    }
+
+    @JvmStatic
+    @UDFunction(description = "Chat with the LLM model")
+    fun chat(dom: ValueDom, prompt: String): String {
+        return session.chat(dom.element, prompt).content
+    }
+
+    @JvmStatic
+    @UDFunction(description = "Extract fields from the given HTML content with the LLM model")
+    fun extract(dom: ValueDom, fieldDescriptions: String): String {
+        val prompt =
+            """
+Extract fields from the given HTML content, return the result in JSON format like:
+
+```json
+{
+  "field1": "value1",
+  "field2": "value2"
+}
+```
+
+Field descriptions: 
+$fieldDescriptions
+
+""".trimIndent()
+
+        return session.chat(dom.element, prompt).content
+    }
+}
