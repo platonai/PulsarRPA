80047c6649bf7fc72b997b2dfe436281b5723d10
platon.AI <galaxyeye@live.cn>
2025-05-03 09:22:17 +0800

    refactor(pulsar-llm): 优化 LLM 模型加载和配置
    - 添加对 OpenAI API 密钥的额外检查
    - 改进错误处理和用户提示，包括指向文档的链接
    - 优化 DeepSeek 和 OpenAI 模型的配置加载- 增加请求重试次数和设置超时时间
    
    Signed-off-by: platon.AI <galaxyeye@live.cn>

--- pulsar-third/pulsar-llm/src/main/kotlin/ai/platon/pulsar/external/ChatModelFactory.kt
+++ pulsar-third/pulsar-llm/src/main/kotlin/ai/platon/pulsar/external/ChatModelFactory.kt
@@ -3,6 +3,7 @@ package ai.platon.pulsar.external
 import ai.platon.pulsar.common.config.ImmutableConfig
 import ai.platon.pulsar.common.warn
 import ai.platon.pulsar.external.impl.ChatModelImpl
+import com.alibaba.dashscope.utils.Constants.apiKey
 import com.ibm.icu.impl.CurrencyData.provider
 import dev.langchain4j.model.openai.OpenAiChatModel
 import dev.langchain4j.model.zhipu.ZhipuAiChatModel
@@ -21,6 +22,11 @@ object ChatModelFactory {
         // deepseek official
         val deepseekAPIKey = conf["DEEPSEEK_API_KEY"]
         if (deepseekAPIKey != null) {
+            return true
+        }
+
+        val openaiAPIKey = conf["OPENAI_API_KEY"]
+        if (openaiAPIKey != null) {
             return true
         }
 
@@ -45,21 +51,22 @@ object ChatModelFactory {
         // so the following keys are equal:
         // - DEEPSEEK_API_KEY, deepseek.apiKey
         val deepseekAPIKey = conf["DEEPSEEK_API_KEY"]
-        val deepseekModelName = conf["DEEPSEEK_MODEL_NAME"] ?: conf["LLM_NAME"] ?: "deepseek-chat"
         if (deepseekAPIKey != null) {
+            val deepseekModelName = conf["DEEPSEEK_MODEL_NAME"] ?: conf["LLM_NAME"] ?: "deepseek-chat"
             return getOrCreate("deepseek", deepseekModelName, deepseekAPIKey, conf)
         }
 
         val openaiAPIKey = conf["OPENAI_API_KEY"]
-        val openaiBaseURL = conf["OPENAI_BASE_URL"] ?: "https://api.openai.com/v1/chat/completions"
-        val openaiModelName = conf["OPENAI_MODEL_NAME"] ?: conf["LLM_NAME"] ?: "gpt-4o-mini"
         if (openaiAPIKey != null) {
+            val openaiBaseURL = conf["OPENAI_BASE_URL"] ?: "https://api.openai.com/v1/chat/completions"
+            val openaiModelName = conf["OPENAI_MODEL_NAME"] ?: conf["LLM_NAME"] ?: "gpt-4o"
             return getOrCreateOpenAICompatibleModel(openaiModelName, openaiAPIKey, openaiBaseURL, conf)
         }
 
-        val provider = conf["llm.provider"] ?: throw IllegalArgumentException("llm.provider is not set")
-        val modelName = conf["llm.name"] ?: throw IllegalArgumentException("llm.name is not set")
-        val apiKey = conf["llm.apiKey"] ?: throw IllegalArgumentException("llm.apiKey is not set")
+        val documentPath = "https://github.com/platonai/PulsarRPA/blob/master/docs/config/llm/llm-config-advanced.md"
+        val provider = requireNotNull(conf["llm.provider"])  { "llm.provider is not set, see $documentPath" }
+        val modelName = requireNotNull(conf["llm.name"]) { "llm.name is not set, see $documentPath" }
+        val apiKey = requireNotNull(conf["llm.apiKey"]) { "llm.apiKey is not set, see $documentPath" }
 
         return getOrCreate(provider, modelName, apiKey, conf)
     }
@@ -76,7 +83,6 @@ object ChatModelFactory {
     @Throws(IllegalArgumentException::class)
     fun getOrCreate(provider: String, modelName: String, apiKey: String, conf: ImmutableConfig) =
         getOrCreateModel0(provider, modelName, apiKey, conf)
-
 
     /**
      * Create a default model.
@@ -191,7 +197,8 @@ object ChatModelFactory {
             .modelName(modelName)
             .logRequests(false)
             .logResponses(true)
-            .maxRetries(1)
+            .maxRetries(2)
+            .timeout(Duration.ofSeconds(90))
             .build()
         return ChatModelImpl(lm, conf)
     }
