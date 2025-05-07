a449e15e4e5a9f7ea249ecd54237239a8040f31e
platon.AI <galaxyeye@live.cn>
2025-05-07 22:47:29 +0800

    test(pulsar-protocol-playwright): 添加 Playwright 驱动测试用例
    - 在 PlaywrightDriverTest 类中添加测试用例
    - 验证浏览器类型是否为 PLAYWRIGHT_CHROME
    - 检查浏览器设置和启动过程
    
    Signed-off-by: platon.AI <galaxyeye@live.cn>

--- pulsar-plugins/pulsar-protocol-playwright/src/test/kotlin/ai/platon/pulsar/protocol/browser/driver/playwright/PlaywrightDriverTest.kt
+++ pulsar-plugins/pulsar-protocol-playwright/src/test/kotlin/ai/platon/pulsar/protocol/browser/driver/playwright/PlaywrightDriverTest.kt
@@ -9,7 +9,10 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class PlaywrightDriverTest {
 
@@ -22,6 +25,8 @@ class PlaywrightDriverTest {
 
     @BeforeEach
     fun setup() {
+        assertEquals("PLAYWRIGHT_CHROME", browserId.browserType.name)
+
         launcherOptions.browserSettings.confuser.reset()
         browser = PlaywrightBrowserLauncher().launch(browserId, launcherOptions, chromeOptions)
         driver = browser.newDriver()
