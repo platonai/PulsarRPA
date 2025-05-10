882a2447df5158f7262803525b51286815bbe5ef
platon.AI <galaxyeye@live.cn>
2025-05-10 19:41:23 +0800

    Fix: LoadingWebDriverPoolTest failed on low-spec computers #87

--- pulsar-plugins/pulsar-protocol/src/test/kotlin/ai/platon/pulsar/protocol/browser/driver/LoadingWebDriverPoolTest.kt
+++ pulsar-plugins/pulsar-protocol/src/test/kotlin/ai/platon/pulsar/protocol/browser/driver/LoadingWebDriverPoolTest.kt
@@ -1,12 +1,15 @@
 package ai.platon.pulsar.protocol.browser.driver
 
 import ai.platon.pulsar.common.LinkExtractors
+import ai.platon.pulsar.common.Runtimes
+import ai.platon.pulsar.common.alwaysTrue
 import ai.platon.pulsar.common.browser.BrowserType
 import ai.platon.pulsar.common.config.ImmutableConfig
 import ai.platon.pulsar.common.sleepSeconds
 import ai.platon.pulsar.protocol.browser.DefaultWebDriverPoolManager
 import ai.platon.pulsar.protocol.browser.driver.playwright.PlaywrightDriver
 import ai.platon.pulsar.skeleton.common.AppSystemInfo
+import ai.platon.pulsar.skeleton.crawl.fetch.driver.Browser
 import ai.platon.pulsar.skeleton.crawl.fetch.driver.WebDriver
 import ai.platon.pulsar.skeleton.crawl.fetch.privacy.BrowserId
 import kotlinx.coroutines.runBlocking
@@ -18,14 +21,24 @@ import kotlin.test.Test
 
 class LoadingWebDriverPoolTest {
     private val config = ImmutableConfig()
-    // private val browserId = BrowserId.createRandomTemp(BrowserType.PLAYWRIGHT_CHROME)
-    private val browserId = BrowserId.createRandomTemp(BrowserType.PLAYWRIGHT_CHROME)
+    private lateinit var browserId: BrowserId
     private val poolManager = DefaultWebDriverPoolManager(config)
     private lateinit var pool: LoadingWebDriverPool
     private val seeds = LinkExtractors.fromResource("seeds/seeds.txt")
 
+    fun checkPlaywrightAvailable(): Boolean {
+        val outputs = Runtimes.exec("playwright --version")
+        return outputs.any { it.contains("Version") }
+    }
+
     @BeforeEach
     fun setup() {
+        browserId = if (checkPlaywrightAvailable()) {
+            BrowserId.createRandomTemp(BrowserType.PLAYWRIGHT_CHROME)
+        } else {
+            BrowserId.createRandomTemp()
+        }
+
         pool = poolManager.createUnmanagedDriverPool(browserId)
     }
 
