65cd6c55ee5680a2778e94d287af1814dc8a7bad
platon.AI <galaxyeye@live.cn>
2025-05-13 21:12:53 +0800

    fix(browser): properly set BROWSER_CONTEXT_MODE
    - Deprecate withSequentialBrowsers method in favor of withBrowserContextMode
    - Update ContinuousCrawler example to use withBrowserContextMode
    - Add deprecated method withSequentialBrowsers as a private method

--- pulsar-tools/pulsar-browser/src/main/kotlin/ai/platon/pulsar/browser/common/BrowserSettings.kt
+++ pulsar-tools/pulsar-browser/src/main/kotlin/ai/platon/pulsar/browser/common/BrowserSettings.kt
@@ -166,10 +166,6 @@ open class BrowserSettings constructor(
         @JvmStatic
         fun withSequentialBrowsers(): Companion = withSequentialBrowsersInternal(BrowserType.PULSAR_CHROME, 10)
 
-        @Deprecated("Use withBrowserContextMode instead, this method will be a private method",
-            ReplaceWith("withBrowserContextMode(contextMode, browserType)"))
-        fun withSequentialBrowsers(browserType: BrowserType): Companion = withSequentialBrowsersInternal(browserType, 10)
-
         fun withSequentialBrowsers(maxAgents: Int): Companion {
             return withSequentialBrowsersInternal(BrowserType.PULSAR_CHROME, maxAgents)
         }
@@ -178,8 +174,13 @@ open class BrowserSettings constructor(
             return withSequentialBrowsersInternal(browserType, maxAgents)
         }
 
+        @Deprecated("Use withBrowserContextMode instead, this method will be a private method",
+            ReplaceWith("withBrowserContextMode(contextMode, browserType)"))
+        fun withSequentialBrowsers(browserType: BrowserType): Companion = withSequentialBrowsersInternal(browserType, 10)
+
         private fun withSequentialBrowsersInternal(browserType: BrowserType, maxAgents: Int): Companion {
             withBrowser(browserType)
+            System.setProperty(BROWSER_CONTEXT_MODE, "SEQUENTIAL")
             System.setProperty(MAX_SEQUENTIAL_PRIVACY_AGENT_NUMBER, "$maxAgents")
             val clazz = "ai.platon.pulsar.skeleton.crawl.fetch.privacy.SequentialPrivacyAgentGenerator"
             System.setProperty(PRIVACY_AGENT_GENERATOR_CLASS, clazz)
