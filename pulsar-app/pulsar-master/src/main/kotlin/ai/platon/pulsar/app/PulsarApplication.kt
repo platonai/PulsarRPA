1934d7d064d0cca2e32a50c015f82140741eaf83
platon.AI <galaxyeye@live.cn>
2025-05-16 15:40:54 +0800

    feat(config): enhance configuration management and add property sources logger
    - Add support for external configuration files (application-private.yml and application-private.properties)
    - Introduce ConfigSourceLogger to log property sources at application startup
    - Update PulsarApplication

--- pulsar-app/pulsar-master/src/main/kotlin/ai/platon/pulsar/app/PulsarApplication.kt
+++ pulsar-app/pulsar-master/src/main/kotlin/ai/platon/pulsar/app/PulsarApplication.kt
@@ -1,12 +1,14 @@
 package ai.platon.pulsar.app
 
 import ai.platon.pulsar.boot.autoconfigure.PulsarContextInitializer
+import jakarta.annotation.PostConstruct
 import org.h2.tools.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.ImportResource
+import java.nio.file.Paths
 import java.sql.SQLException
 
 @SpringBootApplication
