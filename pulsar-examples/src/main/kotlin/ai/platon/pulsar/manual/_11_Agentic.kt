2ad3000bb611968d766116271429ef0860b4bdd2
platon.AI <galaxyeye@live.cn>
2025-11-22 15:59:15 +0800

    refactor: rename variables from problems to tasks for improved clarity

--- pulsar-examples/src/main/kotlin/ai/platon/pulsar/manual/_11_Agentic.kt
+++ pulsar-examples/src/main/kotlin/ai/platon/pulsar/manual/_11_Agentic.kt
@@ -6,16 +6,16 @@ class ProblemResolver {
     val agent = AgenticContexts.getOrCreateAgent()
 
     suspend fun run() {
-        val problems = """
+        val tasks = """
 go to https://news.ycombinator.com/news , search for browser and read top 5 articles and give me a summary
 go to amazon.com and search for pens to draw on whiteboards
 打开百度查找厦门岛旅游景点，给出一个总结
 go to https://news.ycombinator.com/news , open the 4-th articles in new tab
 go to https://news.ycombinator.com/news , read top 3 articles and give me a summary
-        """.split("\n").filter { it.isNotBlank() }
+        """.lines().filter { it.isNotBlank() }
 
-        val problem = problems[0]
-        agent.run(problem)
+        val task = tasks[0]
+        agent.run(task)
     }
 }
 
