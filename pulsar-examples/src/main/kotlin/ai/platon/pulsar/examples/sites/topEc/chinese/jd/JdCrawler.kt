1b4eca67bff92b76182eac61f7f9ca874a441428
platon.AI <galaxyeye@live.cn>
2025-12-26 18:08:57 +0800

    refactor: enhance page loading and parsing in main function

--- pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/topEc/chinese/jd/JdCrawler.kt
+++ pulsar-examples/src/main/kotlin/ai/platon/pulsar/examples/sites/topEc/chinese/jd/JdCrawler.kt
@@ -5,10 +5,15 @@ import ai.platon.pulsar.skeleton.context.PulsarContexts
 
 fun main() {
     BrowserSettings.withSystemDefaultBrowser()
-    
+
     val portalUrl = "https://list.jd.com/list.html?cat=652,12345,12349"
     val args = "-i 1h -ii 5s -ol a[href~=item] -ignoreFailure"
 
     val session = PulsarContexts.createSession()
-    session.loadOutPages(portalUrl, args)
+    val pages = session.loadOutPages(portalUrl, args)
+
+    pages.forEach {
+        val document = session.parse(it)
+        println(document.title)
+    }
 }
