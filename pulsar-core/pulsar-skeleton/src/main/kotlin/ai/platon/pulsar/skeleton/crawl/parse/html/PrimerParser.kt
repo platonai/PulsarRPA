fe4398ab7d8a584abbd8dfd87eaac5ca2d9d0d98
platon.AI <galaxyeye@live.cn>
2026-01-11 20:42:35 +0800

    fix: remove unused ICU4J dependency and related encoding detection logic

--- pulsar-core/pulsar-skeleton/src/main/kotlin/ai/platon/pulsar/skeleton/crawl/parse/html/PrimerParser.kt
+++ pulsar-core/pulsar-skeleton/src/main/kotlin/ai/platon/pulsar/skeleton/crawl/parse/html/PrimerParser.kt
@@ -3,11 +3,8 @@ package ai.platon.pulsar.skeleton.crawl.parse.html
 import ai.platon.pulsar.common.Strings
 import ai.platon.pulsar.common.config.ImmutableConfig
 import ai.platon.pulsar.persist.WebPage
-import ai.platon.pulsar.skeleton.common.EncodingDetector
 import ai.platon.pulsar.skeleton.crawl.parse.Parser
-import org.apache.tika.Tika
 import org.slf4j.LoggerFactory
-import java.util.*
 
 /**
  * A very simple DOM parser.
@@ -15,18 +12,7 @@ import java.util.*
 class PrimerParser(val conf: ImmutableConfig) {
     private val logger = LoggerFactory.getLogger(Parser::class.java)
     private val tracer = logger.takeIf { it.isTraceEnabled }
-    
-    private var encodingDetector = EncodingDetector(conf)
 
-    fun detectEncoding(page: WebPage) {
-        val encoding = encodingDetector.sniffEncoding(page)
-        if (encoding != null && encoding.isNotEmpty()) {
-            page.encoding = encoding
-        } else {
-            logger.warn("Failed to detect encoding, url: " + page.url)
-        }
-    }
-    
     @Throws(Exception::class)
     fun parseHTMLDocument(page: WebPage): ParseContext {
         tracer?.trace(
@@ -34,14 +20,10 @@ class PrimerParser(val conf: ImmutableConfig) {
             page.id, Strings.compactFormat(page.contentLength),
             page.protocolStatus, page.htmlIntegrity, page.url
         )
-        
-        if (page.encoding == null) {
-            detectEncoding(page)
-        }
-        
+
         val jsoupParser = JsoupParser(page, conf)
         jsoupParser.parse()
-        
+
         return ParseContext(page, jsoupParser.document)
     }
 }
